5edb3d74bebef1ab354a694ba85884b6c815bd36
Carlos Rangel <charlesleimaginant@gmail.com>
2022-01-19 22:17:36 -0600

    Followed the 'Output Management' part of the tutorial

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
+import printMe from './print.js';
+
 
 function component() {
     const element = document.createElement('div');
+    const btn = document.createElement('button');
   
-    // Lodash, cnow imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // Add the image to our existing div.
-    const myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console';
+    btn.onclick = printMe;
 
-    element.appendChild(myIcon);
+    element.appendChild(btn);
   
     return element;
 }
